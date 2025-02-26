dc6afc1d81738596f83b0361ba5c81d883c294b0
cjpark-sapcsa <chanpark@microsoft.com>
2025-02-26 14:27:31 +0800

    Updated project files and removed old workflows

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -9,22 +9,23 @@ def create_chat_completion(messages):
     - The Azure OpenAI endpoint and deployment name are stored in Streamlit secrets."""
 
     # Retrieve secrets from the Streamlit secret store.
-    # This is a secure way to store sensitive information that you don't want to expose in your code.
-    # Learn more about Streamlit secrets here: https://docs.streamlit.io/develop/concepts/connections/secrets-management
-    # The secrets themselves are stored in the .streamlit/secrets.toml file.
-
     token_provider = get_bearer_token_provider(
         DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
     )
     
     aoai_endpoint = st.secrets["aoai"]["endpoint"]
     aoai_deployment_name = st.secrets["aoai"]["deployment_name"]
+    
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
 
     client = openai.AzureOpenAI(
         azure_ad_token_provider=token_provider,
         api_version="2024-06-01",
-        azure_endpoint = aoai_endpoint
+        azure_endpoint=aoai_endpoint
     )
+    
     # Create and return a new chat completion request
     return client.chat.completions.create(
         model=aoai_deployment_name,
@@ -32,7 +33,22 @@ def create_chat_completion(messages):
             {"role": m["role"], "content": m["content"]}
             for m in messages
         ],
-        stream=True
+        stream=True,
+        extra_body={
+            "data_sources": [
+                {
+                    "type": "azure_search",
+                    "parameters": {
+                        "endpoint": search_endpoint,
+                        "index_name": search_index_name,
+                        "authentication": {
+                            "type": "api_key",
+                            "key": search_key
+                        }
+                    }
+                }
+            ]
+        }
     )
 
 def handle_chat_prompt(prompt):
@@ -45,9 +61,6 @@ def handle_chat_prompt(prompt):
         st.markdown(prompt)
  
     # Send the user's prompt to Azure OpenAI and display the response
-    # The call to Azure OpenAI is handled in create_chat_completion()
-    # This function loops through the responses and displays them as they come in.
-    # It also appends the full response to the chat history.
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
@@ -56,17 +69,18 @@ def handle_chat_prompt(prompt):
                 full_response += (response.choices[0].delta.content or "")
                 message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
+    
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 def main():
     """Main function for the Chat with Data Streamlit app."""
 
     st.write(
-    """
-    # Chat with Data
+        """
+        # Chat with Data
 
-    This Streamlit dashboard is intended to show off capabilities of Azure OpenAI, including integration with AI Search.
-    """
+        This Streamlit dashboard is intended to show off capabilities of Azure OpenAI, including integration with AI Search.
+        """
     )
 
     # Initialize chat history
